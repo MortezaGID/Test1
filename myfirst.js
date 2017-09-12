8c3f011978f2c28089c619c4d71df451567487e3
MortezaGID <MortezaGID@gmail.com>
2017-09-12 10:56:37 +0430

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,11 @@
 var http = require('http');
-
+const net = require('net');
+const client = net.connect({port: 80, host:"google.com"}, () => {
+  console.log('MyIP='+client.localAddress);
+  console.log('MyPORT='+client.localPort);
+});
 http.createServer(function (req, res) {
+
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
 }).listen(8080);
